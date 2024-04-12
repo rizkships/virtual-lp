a9a7aa2105a66fa2553556e0d211f62175fb6ccc
rizk <rizkdotai@gmail.com>
2024-04-12 10:41:07 +0200

    add basic navbar style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Navbar/>
+    </>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80'>
+
+    </nav>
+  )
+}
+
+export default Navbar
