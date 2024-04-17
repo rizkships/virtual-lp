03369dae835a3b7c3d3d14ad3967992df62432df
rizk <rizkdotai@gmail.com>
2024-04-17 19:39:56 +0200

    add and style nav cta button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,21 @@ const Navbar = () => {
                     <img className='h-10 w-10 mr-2' src={logo} alt=''/>
                     <span className='text-xl tracking-tight'>VirtualR</span>
                 </div>
-                <ul className='lg-flex ml-14 space-x-12'>
+                <ul className='lg-flex ml-14 space-x-12'> {/* I removed hidden! */}
                     {navItems.map((item, index) => (
                         <li key = {index}>
                             <a href={item.href}>{item.label}</a>
                         </li>
                     ))}
                 </ul>
+                <div className='hidden lg:flex justify-center space-x-12 items-center'>
+                    <a href="#" className='py-2 px-3 border rounded-md'>
+                        Sign In
+                    </a>
+                    <a href='#' className='bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md'>
+                        Create an account
+                    </a>
+                </div>
             </div>
         </div>
     </nav>
