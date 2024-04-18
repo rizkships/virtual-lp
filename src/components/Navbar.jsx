f8923360af9e39b5517a9bcc85e932734c3492fb
rizk <rizkdotai@gmail.com>
2024-04-18 15:01:02 +0200

    fix nav spacing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className='sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80'>
         <div className='container px-4 mx-auto relative text-sm'>
-            <div className='flex justify-center items-center'>
+            <div className='flex justify-between items-center'>
                 <div className='flex items-center flex-shrink-0'>
                     <img className='h-10 w-10 mr-2' src={logo} alt=''/>
                     <span className='text-xl tracking-tight'>VirtualR</span>
