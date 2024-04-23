724db33205044dc2724fb84b5fdf58e6d66a5054
rizk <rizkdotai@gmail.com>
2024-04-23 07:43:08 +0200

    add cta buttons for mobile nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -47,6 +47,14 @@ const Navbar = () => {
                             </li>
                         ))}
                     </ul>
+                    <div className='flex space-x-6'>
+                        <a href='#' className='py-2 px-3 border rounded-md'>
+                            Sign In
+                        </a>
+                        <a href="#" className='py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800'>
+                            Create an account
+                        </a>
+                    </div>
                 </div>
             )}
         </div>
