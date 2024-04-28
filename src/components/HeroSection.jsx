f4022b50b954740fc520a517c1f3fc580f511e93
rizk <rizkdotai@gmail.com>
2024-04-28 16:52:29 +0300

    add and style hero secondary text

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -9,6 +9,9 @@ const HeroSection = () => {
           {" "}
           for developers </span>
       </h1>
+      <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
+        Empower your creativity and bring your VR app ideas to life with our intuitive development tools. Get started today and turn your imagination into immersive reality!
+      </p>
     </div>
   )
 }
