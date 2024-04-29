8b75bd8d017e0edeaaa1c6876a70f0cbc96107f7
rizk <rizkdotai@gmail.com>
2024-04-29 08:11:30 +0300

    add hero CTA buttons

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,6 +12,14 @@ const HeroSection = () => {
       <p className="mt-10 text-lg text-center text-neutral-500 max-w-4xl">
         Empower your creativity and bring your VR app ideas to life with our intuitive development tools. Get started today and turn your imagination into immersive reality!
       </p>
+      <div className="flex justify-center my-10">
+        <a href="#" className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md">
+          start for free
+        </a>
+        <a href="#" className="py-3 px-4 mx-3 rounded-md border">
+          Documentation
+        </a>
+      </div>
     </div>
   )
 }
