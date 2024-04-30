37cf7b7cd4f1da706772326182200b13c0609ce6
rizk <rizkdotai@gmail.com>
2024-04-30 11:34:26 +0300

    add and style hero videos

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -20,6 +20,16 @@ const HeroSection = () => {
           Documentation
         </a>
       </div>
+      <div className="flex mt-10 justify-center">
+        <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
+          <source src={video1} type="video/mp4" />
+          Your browser does not support the video tag. 
+        </video>
+        <video autoPlay loop muted className="rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4">
+          <source src={video2} type="video/mp4" />
+          Your browser does not support the video tag. 
+        </video>
+      </div>
     </div>
   )
 }
