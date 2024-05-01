8b45e2a952585e315f141417cd28a73a5c9b26a3
rizk <rizkdotai@gmail.com>
2024-05-01 12:53:20 +0300

    add feature section and style heading

---
+++ src/components/FeatureSection.jsx
@@ -0,0 +1,17 @@
+
+const FeatureSection = () => {
+  return (
+    <div className="relative mt-20 border-b border-neutral-800 min-h-[800px]">
+        <div className="text-center">
+            <span className="bg-neutral-900 text-orange-500 rounded-full h-6 text-sm font-medium px-2 py-1 uppercase">
+                Features
+            </span>
+            <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
+                Easily build <span className="bg-gradient-to-r from-orange-500 to orange-800 text-transparent bg-clip-text">{" "}your code</span>
+            </h2>
+        </div>
+    </div>
+  )
+}
+
+export default FeatureSection
