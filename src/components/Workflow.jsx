dbf6d4c4fb47a1ca9622631cfe6a2aa36a29302c
rizk <rizkdotai@gmail.com>
2024-05-06 17:36:57 +0300

    map over checklist icon and items to display

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,5 +1,6 @@
 import { CheckCircle2 } from "lucide-react"
 import codeImg from "../assets/code.jpg";
+import { checklistItems } from "../constants";
 
 const Workflow = () => {
   return (
@@ -13,7 +14,17 @@ const Workflow = () => {
                 <img src={codeImg} alt="Code" />
             </div>
             <div className="pt-12 w-full lg:w-12">
-                
+            {checklistItems.map((item, index) => (
+            <div key={index} className="flex mb-12">
+              <div className="text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
+                <CheckCircle2 />
+              </div>
+              <div>
+                <h5 className="mt-1 mb-2 text-xl">{item.title}</h5>
+                <p className="text-md text-neutral-500">{item.description}</p>
+              </div>
+            </div>
+          ))}
             </div>
         </div>
     </div>
