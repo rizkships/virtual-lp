6c4baa61284ab9756cb2377249a56b49d4045687
rizk <rizkdotai@gmail.com>
2024-05-07 15:57:12 +0300

    fix check items layout

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,20 +1,22 @@
-import { CheckCircle2 } from "lucide-react"
+import { CheckCircle2 } from "lucide-react";
 import codeImg from "../assets/code.jpg";
 import { checklistItems } from "../constants";
 
 const Workflow = () => {
   return (
     <div className="mt-20">
-        <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
-            Accelerate your{" "}
-            <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">coding workflow.</span>
-        </h2>
-        <div className="flex flex-wrap justify-center">
-            <div className="p-2 w-full lg:w-1/2">
-                <img src={codeImg} alt="Code" />
-            </div>
-            <div className="pt-12 w-full lg:w-12">
-            {checklistItems.map((item, index) => (
+      <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center mt-6 tracking-wide">
+        Accelerate your{" "}
+        <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+          coding workflow.
+        </span>
+      </h2>
+      <div className="flex flex-wrap justify-center">
+        <div className="p-2 w-full lg:w-1/2">
+          <img src={codeImg} alt="Coding" />
+        </div>
+        <div className="pt-12 w-full lg:w-1/2">
+          {checklistItems.map((item, index) => (
             <div key={index} className="flex mb-12">
               <div className="text-green-400 mx-6 bg-neutral-900 h-10 w-10 p-2 justify-center items-center rounded-full">
                 <CheckCircle2 />
@@ -25,10 +27,10 @@ const Workflow = () => {
               </div>
             </div>
           ))}
-            </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Workflow
+export default Workflow;
