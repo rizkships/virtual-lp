48032a6b05c2aa0500ffc54cbec222a6cfd951bd
rizk <rizkdotai@gmail.com>
2024-05-08 15:34:51 +0300

    add pricing section

---
+++ src/components/pricing.jsx
@@ -0,0 +1,14 @@
+import { CheckCircle2 } from "lucide-react"
+import { pricingOptions } from "../constants"
+
+const pricing = () => {
+  return (
+    <div className="mt-20">
+        <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-8 tracking-wider">
+            Pricing
+        </h2>
+    </div>
+  )
+}
+
+export default pricing
