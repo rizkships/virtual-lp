80d5579ce97c51df71612c71896318f04dceed9a
rizk <rizkdotai@gmail.com>
2024-05-11 20:20:25 +0300

    map and style pricing card titles

--- src/components/pricing.jsx
+++ src/components/pricing.jsx
@@ -7,6 +7,16 @@ const pricing = () => {
         <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-8 tracking-wider">
             Pricing
         </h2>
+        <div className="flex flex-wrap">
+            {pricingOptions.map((option, index) => (
+                <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-2">
+                    <div className="p-10 border border-neutral-700 rounded-xl">
+                        <p>{option.title}</p>
+                    </div>
+                </div>
+            ))}
+
+        </div>
     </div>
   )
 }
