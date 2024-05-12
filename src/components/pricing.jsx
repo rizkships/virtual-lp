09313d41bd641f0204276b027cd0a379484f9c3b
rizk <rizkdotai@gmail.com>
2024-05-12 15:34:24 +0300

    add and style most pop section

--- src/components/pricing.jsx
+++ src/components/pricing.jsx
@@ -12,6 +12,11 @@ const pricing = () => {
                 <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-2">
                     <div className="p-10 border border-neutral-700 rounded-xl">
                         <p>{option.title}</p>
+                        {option.title === "Pro" && (
+                            <span className="bg-gradient-to-r from-orange-500 to-red-400 text-transparent bg-clip-text text-xl mb-4 ml-2">
+                            (Most Popular)
+                          </span>
+                        )}
                     </div>
                 </div>
             ))}
