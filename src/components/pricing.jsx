29b308a56e7d9baa4525123431358012b82167d6
rizk <rizkdotai@gmail.com>
2024-05-14 18:39:17 +0300

    add pricing

--- src/components/pricing.jsx
+++ src/components/pricing.jsx
@@ -11,12 +11,16 @@ const pricing = () => {
             {pricingOptions.map((option, index) => (
                 <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-2">
                     <div className="p-10 border border-neutral-700 rounded-xl">
-                        <p>{option.title}</p>
+                        <p className="text-4xl mb-8">{option.title}
                         {option.title === "Pro" && (
                             <span className="bg-gradient-to-r from-orange-500 to-red-400 text-transparent bg-clip-text text-xl mb-4 ml-2">
                             (Most Popular)
                           </span>
                         )}
+                        </p>
+                        <p className="mb-8">
+                            <span>{option.price}</span>
+                        </p>
                     </div>
                 </div>
             ))}
