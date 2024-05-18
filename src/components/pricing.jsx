503ff0207186dc0370a963cc1247cb624b6f5009
rizk <rizkdotai@gmail.com>
2024-05-18 15:01:42 +0300

    add subscribe button

--- src/components/pricing.jsx
+++ src/components/pricing.jsx
@@ -28,8 +28,14 @@ const pricing = () => {
                              <CheckCircle2 />
                              <span className="ml-2">{feature}</span>
                             </li>
-                ))}
-              </ul>
+                             ))}
+                        </ul>
+                        <a
+                         href="#"
+                         className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-orange-900 border border-orange-900 rounded-lg transition duration-200"
+                        >
+                            Subscribe
+                        </a>
                     </div>
                 </div>
             ))}
