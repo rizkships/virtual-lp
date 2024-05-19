64ba84f302ccfa1b2fe685f96b1661f372893708
rizk <rizkdotai@gmail.com>
2024-05-19 13:59:25 +0300

    add testimonials skeleton on page

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,10 @@
+import { testimonials } from "../constants";
+
+const Testimonials = () => {
+    return (
+        <>
+        </>
+    )
+};
+
+export default Testimonials;
--- src/components/pricing.jsx
+++ src/components/pricing.jsx
@@ -1,7 +1,7 @@
 import { CheckCircle2 } from "lucide-react"
 import { pricingOptions } from "../constants"
 
-const pricing = () => {
+const Pricing = () => {
   return (
     <div className="mt-20">
         <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-8 tracking-wider">
@@ -45,4 +45,4 @@ const pricing = () => {
   )
 }
 
-export default pricing
+export default Pricing
