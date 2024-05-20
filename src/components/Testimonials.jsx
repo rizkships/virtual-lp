43e2c4794b610b8fc6521716bb39eefd5683960e
rizk <rizkdotai@gmail.com>
2024-05-20 17:27:56 +0300

    add and style testimonial header section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,8 +2,11 @@ import { testimonials } from "../constants";
 
 const Testimonials = () => {
     return (
-        <>
-        </>
+        <div className="mt-20 tracking-wide">
+      <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-10 lg:my-20">
+        What People are saying
+      </h2>
+      </div>
     )
 };
 
