dd69bee3a0f4405ebe02b3ba5df2ecd8ef2f3061
rizk <rizkdotai@gmail.com>
2024-05-24 15:11:25 +0300

    style main footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,8 @@ import { resourcesLinks, platformLinks, communityLinks } from "../constants";
 
 const Footer = () => {
     return (
-        <></>
+        <footer className="mt-20 border-t py-10 border-neutral-700">
+        </footer>
     )
 }
 
