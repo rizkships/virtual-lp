337cc2fd9d48befa9379c4bdc516e606244247be
rizk <rizkdotai@gmail.com>
2024-05-25 14:56:45 +0300

    add footer grid

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,9 @@ import { resourcesLinks, platformLinks, communityLinks } from "../constants";
 const Footer = () => {
     return (
         <footer className="mt-20 border-t py-10 border-neutral-700">
+            <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+                
+            </div>
         </footer>
     )
 }
