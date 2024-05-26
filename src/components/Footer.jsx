f7ed408ba842ba05f487c0055ff5c93007da5a22
rizk <rizkdotai@gmail.com>
2024-05-26 12:27:35 +0300

    add and style resources heading

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ const Footer = () => {
     return (
         <footer className="mt-20 border-t py-10 border-neutral-700">
             <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
-                
+                <div>
+                    <h3 className="text-md font-semibold mb-4">Resources</h3>
+                </div>
             </div>
         </footer>
     )
