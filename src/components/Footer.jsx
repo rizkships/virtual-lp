e3d3416629607b8d3d1f6d21633e7b3f728e40ea
rizk <rizkdotai@gmail.com>
2024-05-27 15:10:28 +0300

    map and style resources links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,16 @@ const Footer = () => {
             <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
                 <div>
                     <h3 className="text-md font-semibold mb-4">Resources</h3>
+                    <ul className="space-y-2">
+                        {resourcesLinks.map((link, index) => (
+                            <li key={index}>
+                                <a href={link.href} className="text-neutral-300 hover:text-white">
+                                    {link.text}
+                                </a>
+                            </li>
+                        ))}
+
+                    </ul>
                 </div>
             </div>
         </footer>
