2db4f9143d6aa8721815084d2dd29753730e9c99
rizk <rizkdotai@gmail.com>
2024-05-28 19:47:37 +0300

    add and style platform links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,6 +17,21 @@ const Footer = () => {
 
                     </ul>
                 </div>
+                <div>
+                    <h3 className="text-md font-semibold mb-4">Platform</h3>
+                        <ul className="space-y-2">
+                        {platformLinks.map((link, index) => (
+                            <li key={index}>
+                <a
+                  href={link.href}
+                  className="text-neutral-300 hover:text-white"
+                >
+                  {link.text}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
             </div>
         </footer>
     )
