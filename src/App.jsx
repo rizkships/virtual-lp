cd94751c10b73285da3261120353834c3890cc44
rizk <rizkdotai@gmail.com>
2024-05-29 14:18:33 +0300

    adjust footer col positioning

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import FeatureSection from './components/FeatureSection';
 import Workflow from './components/Workflow';
-import Pricing from './components/pricing';
+import Pricing from './components/Pricing';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="mt-20 border-t py-10 border-neutral-700">
             <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
-                <div>
+                <div className="mr-auto ml-auto">
                     <h3 className="text-md font-semibold mb-4">Resources</h3>
                     <ul className="space-y-2">
                         {resourcesLinks.map((link, index) => (
@@ -17,7 +17,7 @@ const Footer = () => {
 
                     </ul>
                 </div>
-                <div>
+                <div className="mr-auto ml-auto">
                     <h3 className="text-md font-semibold mb-4">Platform</h3>
                         <ul className="space-y-2">
                         {platformLinks.map((link, index) => (
@@ -32,7 +32,7 @@ const Footer = () => {
             ))}
           </ul>
         </div>
-        <div>
+        <div className="mr-auto ml-auto">
           <h3 className="text-md font-semibold mb-4">Community</h3>
           <ul className="space-y-2">
             {communityLinks.map((link, index) => (
